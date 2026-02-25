728b63cf6266e51828485a7357d032e9760c8d2c
justinsgithub3 <justinsrecallback@gmail.com>
2026-02-25 16:21:13 -0500

    update entire site

---
+++ server.js
@@ -0,0 +1,26 @@
+
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+
+// Get the current filename and directory path
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+// Create the express application
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+const port = 8080;
+
+// Home page
+app.get(['/home', '/'], (request, response) => {
+  response.redirect('/views/home.html');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+})
